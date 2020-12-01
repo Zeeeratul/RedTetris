cb0efb16ba390f159dbe5912a479a59cec0b9169
Célestin Delahaye <c.delahaye@agencelafusee.com>
2020-12-01 13:44:25 +0100

    handle pieces

--- src/client/pages/game/Grid.js
+++ src/client/pages/game/Grid.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useReducer } from 'react'
 import useEventListener from '@use-it/event-listener'
-import { initiateSocket, disconnectSocket, subscribeToEvent, emitToEvent } from '../../middlewares/socket'
-import _, { map } from 'lodash'
+import { emitToEvent, subscribeToEvent } from '../../middlewares/socket'
+import _ from 'lodash'
 import './game.css'
 import Line from './Line'
 
@@ -88,118 +88,196 @@ const checkHorizontalPosition = (futurPosition, grid) => {
     return true
 }
 
+// change the name
+
+const checkFullLine = (line) => {
+    const reducer = (accumulator, currentValue) => accumulator + (currentValue ? 1 : 0)
+    if (line.reduce(reducer, 0) === 10)
+        return true
+    else
+        return false
+}
+
+// change the name
+const clearFullLine = (grid) => {
+    const lineToDelete = []
+    let newGrid = [...grid]
+
+    grid.forEach((line, index) => {
+        if (checkFullLine(line))
+            lineToDelete.push(index)
+    })
+
+    // remove all full line
+    _.reverse(lineToDelete).forEach((index) => {
+        newGrid.splice(index, 1)
+    })
+
+    // add new empty line in the top of grid
+    lineToDelete.forEach(() => {
+        newGrid.unshift(feelLineWithEmpty())
+    })
+
+    return newGrid
+}
+
+const feelLineWithEmpty = () => new Array(10).fill(0)
+
+
+
+const initialState = {
+    grid: initGrid,
+    currentPiece: null,
+    nextPiece: null
+}
+
+const handleRotate = (currentPiece, grid) => {
+    const structureRotated = rotatePiece(currentPiece.structure)
+    const updatedPiece = { ...currentPiece, structure: structureRotated }
+    const newPiecePositions = convertCoords(updatedPiece)
+    if (checkVerticalPosition(newPiecePositions, grid) && checkHorizontalPosition(newPiecePositions, grid))
+        return updatedPiece
+    else
+        return currentPiece   
+
+        // setCurrentPiece(updatedPiece)
+}
+
+
+const handleMoveHorizontal = (x_axis_direction, grid, currentPiece) => {
+    const [x, y] = currentPiece.position
+    const updatedPiece = { ...currentPiece, position: [x + x_axis_direction, y] }
+    const newPiecePositions = convertCoords(updatedPiece)
+    if (checkHorizontalPosition(newPiecePositions, grid))
+        return updatedPiece
+    else
+        return currentPiece
+}
+
+
+const handleMoveBottom = (y_axis_direction, grid, currentPiece) => {
+    const [x, y] = currentPiece.position
+    const updatedPiece = { ...currentPiece, position: [x, y + y_axis_direction] }
+    const newPiecePositions = convertCoords(updatedPiece)
+
+    if (checkVerticalPosition(newPiecePositions, grid))
+        return updatedPiece
+    else {
+        return currentPiece
+    }
+}
+
+const addPieceToTheGrid = (currentPiece, grid) => {
+    const newGrid = [...grid]
+    const piecePositions = convertCoords(currentPiece)
+
+    piecePositions.forEach((part) => {
+        const [x, y] = part
+        newGrid[y][x] = currentPiece.type
+    })
+
+    return clearFullLine(newGrid)
+}
+
+function reducer(state, action) {
+    const { grid, currentPiece, nextPiece } = state;
+
+    if (action.type === 'ArrowRight') {
+        return {
+            ...state,
+            currentPiece: handleMoveHorizontal(1, grid, currentPiece)
+        }
+    } 
+    else if (action.type === 'ArrowLeft') {
+        return {
+            ...state,
+            currentPiece: handleMoveHorizontal(-1, grid, currentPiece)
+        }
+    } 
+    else if (action.type === 'ArrowDown') {
+        const updatedPiece = handleMoveBottom(1, grid, currentPiece)
+        if (updatedPiece.position === currentPiece.position) {
+            return {
+                grid: addPieceToTheGrid(currentPiece, grid),
+                currentPiece: nextPiece,
+                nextPiece: null
+            }
+        }
+        else {
+            return {
+                ...state,
+                currentPiece: updatedPiece
+            }
+        }
+    } 
+    else if (action.type === 'ArrowUp') {
+        const updatedPiece = handleRotate(currentPiece, grid)
+        return {
+            ...state,
+            currentPiece: updatedPiece
+        }
+    } 
+    else if (action.type === 'piece') {
+        return {
+            ...state,
+            currentPiece: action.payload
+        }
+    }
+    else if (action.type === 'next_piece') {
+        return {
+            ...state,
+            nextPiece: action.payload
+        }
+    }
+    // space bar
+    else if (action.type === ' ') {
+        console.log('space nbars')
+        return {
+            ...state,
+            // nextPiece: action.payload
+        }
+    }
+    else {
+        console.log('This key isn\'t supported: ', action.type)
+        return state
+
+    //   throw new Error()
+    }
+  }
 
 
 function Game() {
-    
-    const [grid, setGrid] = useState(() => initGrid)
-    const [currentPiece, setCurrentPiece] = useState(null)
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const { grid, currentPiece, nextPiece } = state
 
     useEffect(() => {
         emitToEvent('piece', '', ({ error, piece }) => {
-            setCurrentPiece(piece)
+            dispatch({ type: 'piece', payload: piece })
         })
+
     }, [])
 
-    const feelLineWithEmpty = () => new Array(10).fill(0)
+    useEffect(() => {
+        if (!nextPiece) {
+            emitToEvent('piece', '', ({ error, piece }) => {
+                dispatch({ type: 'next_piece', payload: piece })
+            })
+        }
+    }, [nextPiece])
 
-    const clearFullLine = (grid) => {
-        const lineToDelete = []
-        let newGrid = [...grid]
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            dispatch({ type: 'ArrowDown' })
+        }, 2000)
 
-        grid.forEach((line, index) => {
-            if (checkFullLine(line))
-                lineToDelete.push(index)
-        })
-
-        // remove all full line
-        _.reverse(lineToDelete).forEach((index) => {
-            newGrid.splice(index, 1)
-        })
-
-        // add new empty line in the top of grid
-        lineToDelete.forEach(() => {
-            newGrid.unshift(feelLineWithEmpty())
-        })
-
-        return newGrid
-        // setGrid(newGrid)
-    }
+        return () => clearInterval(intervalId)
+    }, [])
 
     const piecePositions = currentPiece ? convertCoords(currentPiece) : null
 
-    const handleKeyboard = ({ key }) => {
-        switch (key) {
-            case 'ArrowRight':
-                handleMoveHorizontal(1, grid, currentPiece)
-                break;
-            case 'ArrowLeft':   
-                handleMoveHorizontal(-1, grid, currentPiece)
-                break;
-            case 'ArrowDown':
-                handleMoveBottom(1, grid, currentPiece)
-                break;
-            case 'ArrowUp':
-                handleRotate(currentPiece, grid)
-                break;
-            default:
-                console.log('This key isn\'t supported: ', key)
-                break;
-        }
-    }
-
-    // pure function
-    const addPieceToTheGrid = (currentPiece, grid) => {
-        const newGrid = [...grid]
-        const piecePositions = convertCoords(currentPiece)
-
-        piecePositions.forEach((part) => {
-            const [x, y] = part
-            newGrid[y][x] = currentPiece.type
-        })
-
-        setGrid(clearFullLine(newGrid))
-        emitToEvent('piece', '', ({ error, piece }) => {
-            setCurrentPiece(piece)
-        })
-    }
-
-    const handleMoveHorizontal = (x_axis_direction, grid, currentPiece) => {
-        const [x, y] = currentPiece.position
-        const updatedPiece = { ...currentPiece, position: [x + x_axis_direction, y] }
-        const newPiecePositions = convertCoords(updatedPiece)
-        if (checkHorizontalPosition(newPiecePositions, grid))
-            setCurrentPiece(updatedPiece)
-    }
-
-    const handleMoveBottom = (y_axis_direction, grid, currentPiece) => {
-        const [x, y] = currentPiece.position
-        const updatedPiece = { ...currentPiece, position: [x, y + y_axis_direction] }
-        const newPiecePositions = convertCoords(updatedPiece)
-
-        if (checkVerticalPosition(newPiecePositions, grid))
-            setCurrentPiece(updatedPiece)
-        else
-            addPieceToTheGrid(currentPiece, grid)
-    }
-
-    const handleRotate = (currentPiece, grid) => {
-        const structureRotated = rotatePiece(currentPiece.structure)
-        const updatedPiece = { ...currentPiece, structure: structureRotated }
-        const newPiecePositions = convertCoords(updatedPiece)
-        if (checkVerticalPosition(newPiecePositions, grid) && checkHorizontalPosition(newPiecePositions, grid))
-            setCurrentPiece(updatedPiece)
-    }
-
-    const checkFullLine = (line) => {
-        const reducer = (accumulator, currentValue) => accumulator + (currentValue ? 1 : 0)
-        if (line.reduce(reducer, 0) === 10)
-            return true
-        else
-            return false
-    }
-
-    useEventListener('keydown', handleKeyboard)
+    useEventListener('keydown', ({ key }) => dispatch({
+        type: key
+    }))
 
     return (
         <div>
